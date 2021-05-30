181f0dbd7a72fd3b494cd8b77123cfbd4e35dc36
zciklacekic <zciklacekic@gmail.com>
2021-05-30 22:52:31 +0300

    30.05.2021

---
+++ src/main/java/kodlama/io/northwind/api/controllers/ProductsController.java
@@ -0,0 +1,61 @@
+package kodlama.io.northwind.api.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlama.io.northwind.business.abstracts.ProductService;
+import kodlama.io.northwind.core.utilities.results.DataResult;
+import kodlama.io.northwind.core.utilities.results.Result;
+import kodlama.io.northwind.core.utilities.results.SuccessDataResult;
+import kodlama.io.northwind.entities.concretes.Product;
+import kodlama.io.northwind.entities.dtos.ProductWithCategoryDto;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductsController {
+	
+	private ProductService productService;
+	
+	@Autowired
+	public ProductsController(ProductService productService) {
+		super();
+		this.productService = productService;
+	}
+
+	@GetMapping("/getall")
+	public DataResult<List<Product>> getAll(){
+		return this.productService.getAll();
+	}
+	
+	@PostMapping("/add")
+	public Result add(@RequestBody Product product) {
+		return this.productService.add(product);
+	}
+	@GetMapping("/getByProductName")
+	public DataResult<Product> getByProductName(@RequestParam String productName){
+		return this.productService.getByProductName(productName);
+	}
+	@GetMapping("/getByProductNameAndCategoryId")
+	public DataResult<Product> getByProductNameAndCategory(@RequestParam("productName") String productName, @RequestParam("categoryId") int categoryId){
+		return this.productService.getByProductNameAndCategory(productName, categoryId) ;
+	}
+	@GetMapping("/getByProductNameContains")
+	public DataResult<List<Product>> getByProductNameContains(@RequestParam String productName){
+		return this.productService.getByProductNameContains(productName) ;
+	}
+	@GetMapping("/getAllDesc")
+	public DataResult<List<Product>> getAllSorted() {
+		return this.productService.getAllSorted() ;
+	}
+	@GetMapping("/getProductWithCategoryDetails")
+	public DataResult<List<ProductWithCategoryDto>> getProductWithCategoryDetails(){
+		return this.productService.getProductWithCategoryDetails();
+	}
+}
---
+++ src/main/java/kodlama/io/northwind/core/utilities/results/ErrorDataResult.java
@@ -0,0 +1,20 @@
+package kodlama.io.northwind.core.utilities.results;
+
+public class ErrorDataResult <T> extends DataResult{
+
+		public ErrorDataResult(Object data, String message) {
+			super(data, false, message);
+		}
+		
+		public ErrorDataResult(T data) {
+			super(data,false);
+		}
+		
+		public ErrorDataResult(String message) {
+			super(null, false, message);
+		}
+		
+		public ErrorDataResult() {
+			super(null, false);
+		}
+}
